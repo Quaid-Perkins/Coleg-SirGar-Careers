b6d679098d515fc06fc39acbf56a11511fc812d7
Quaid-Perkins <250244@colegsirgar.ac.uk>
2025-03-11 07:52:50 +0000

    start of contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,13 @@
 import { Meta, MetaProvider, Title } from "@solidjs/meta";
+import { createSignal } from "solid-js";
 
 const Contact = () => {
+  const [name, setName] = createSignal("");
+  const [email, setEmail] = createSignal("");
+  const [message, setMessage] = createSignal("");
+
+  const handleSubmit = () => {};
+
   return (
     <>
       {/* Adding meta data for the page */}
@@ -12,7 +19,23 @@ const Contact = () => {
           content="Contact Coleg SirGâr from Career Compass"
         />
       </MetaProvider>
-      <div>Contact Page</div>
+      <div>
+        <div></div>
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <label for="fname">Name</label>
+          <input type="text" name="name" placeholder="Your name.." />
+          <label for="fname">Email</label>
+          <input type="text" name="email" placeholder="Your Email.." />
+          <label for="subject">Message</label>
+          <textarea
+            name="message"
+            placeholder="Write something.."
+            style="height:200px"
+          ></textarea>
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     </>
   );
 };
